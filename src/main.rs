5631613c026f10f9c6424f54be22d760648ede3b
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-01-30 16:25:28 +0000

    Updated error messages and added list command even if a name is provided

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@ fn main() {
     args.remove(0);
 
     if args.len() >= 2{
-        let todos_input: Vec<&String> = args[1..].iter().collect();
-        process(args[0].as_str(), todos_input);
+        if args[0].eq("list"){
+            print_list();
+        }else{           
+            let todos_input: Vec<&String> = args[1..].iter().collect();
+            process(args[0].as_str(), todos_input);
+        }
     }else if args.len() == 1 {
         if args[0].eq("list"){
             print_list();
